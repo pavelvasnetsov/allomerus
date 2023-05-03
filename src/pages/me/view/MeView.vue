5da8e56fda528f92ad10f7021637fd97a76f6305
Pavel <vasnetsovpavelv@gmail.com>
2023-05-03 15:26:21 +0300

    TASK-7 add base of me page

---
+++ src/pages/me/view/MeView.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container me">
+        <div class="me__header">
+            <div class="me__title"></div>
+            <div class="me__edit-btn"></div>
+        </div>
+        <div class="me__info info">
+            <div class="info__avatar">
+                
+            </div>
+            <div class="info__firstName">
+
+            </div>
+            <div class="info__lastName">
+
+            </div>
+            <div class="info__username">
+                
+            </div>
+            <div class="info__email">
+
+            </div>
+            <div class="info__bio">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: 'MeView',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
